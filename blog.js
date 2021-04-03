b3f8ecb16ff98a97b4568d9ae31e92480a5d5779
JumaGypsum <justuskeya1@gmail.com>
2021-04-03 09:07:22 +0300

    Blog

---
+++ blog.js
@@ -0,0 +1,6 @@
+/*let readBtn = document.getElementById('button').addEventListener('click', readBlog);
+function readBlog(){
+    let paragraph = document.querySelector('p');
+    paragraph.style.display = 'block';
+    readBtn.style.display = '';
+}*/
